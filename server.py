1d84589fe21c0004253efd920cb3fb21513ec3b3
skarthivelan <skarthivelan@gmail.com>
2022-12-25 19:25:44 +0000

    Created Simple portfolio from course
    Created Simple portfolio from course

--- server.py
+++ server.py
@@ -3,5 +3,9 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def root():
     return render_template("index.html")
+
+@app.route("/<string:pageName>")
+def html(pageName):
+    return render_template(pageName)
