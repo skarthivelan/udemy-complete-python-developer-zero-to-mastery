0a20676cbaeacedca00f295f28ec8d7ac357db26
skarthivelan <skarthivelan@gmail.com>
2022-12-26 17:41:31 +0000

    added code for csv file to store contacted form
    added code for csv file to store contacted form

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template, url_for ,request,redirect
-
+import csv
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,11 +13,20 @@ def html(pageName):
 def write_to_file(data):
     with open('ContactedData.txt', mode='a') as database:
         file = database.write(f"\n{data['email']} | {data['subject']} | {data['message']}")
+
+def write_to_csv(data):
+    with open('database.csv', mode='a') as db:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csvFile = csv.writer(db,delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csvFile.writerow([email,subject,message])
+
 @app.route("/submit_form", methods=['POST', 'GET'])
 def formSubmit():
     if request.method == 'POST':
         data = request.form.to_dict()
-        write_to_file(data)
+        write_to_csv(data)
         return redirect('thankyou.html')
     else:
         return "Something went wrong. Try again!"
