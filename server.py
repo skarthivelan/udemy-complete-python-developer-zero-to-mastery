e68f1abfaeff2246d578c60e2133a030c3c4d3f4
skarthivelan <skarthivelan@gmail.com>
2022-12-26 17:20:01 +0000

    Added contact form and stored form data in text file
    Added contact form and stored form data in text file

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, url_for ,request,redirect
 
 app = Flask(__name__)
 
@@ -9,3 +9,16 @@ def root():
 @app.route("/<string:pageName>")
 def html(pageName):
     return render_template(pageName)
+
+def write_to_file(data):
+    with open('ContactedData.txt', mode='a') as database:
+        file = database.write(f"\n{data['email']} | {data['subject']} | {data['message']}")
+@app.route("/submit_form", methods=['POST', 'GET'])
+def formSubmit():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_file(data)
+        return redirect('thankyou.html')
+    else:
+        return "Something went wrong. Try again!"
+
